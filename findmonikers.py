acf1422359fa4c0cf8ed8ce306c41189915613df
alkisl <alk.liv@gmail.com>
2013-02-22 23:45:33 +0200

    Output to txt file

--- findmonikers.py
+++ findmonikers.py
@@ -1,6 +1,13 @@
 import string
 import random
-for k in range(0,5):
+
+
+for k in range(0,4):
 	lst = (random.choice(string.ascii_lowercase) + random.choice('aeiou') for n in xrange(3))
 	str = "".join(lst)
 	print str
+	with open("test.txt", "a") as myfile:
+		myfile.write(str + "\n")
+
+with open("test.txt", "a") as myfile:
+	myfile.write("\n")
